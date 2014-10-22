479e004b472ca3a949c7c94a09e5bebaaad04f8a
luzhe <tcluzhe@hotmail.com>
2014-10-22 16:20:29 +0800

    修改index.html，便于演示链接到播放页功能

--- youmu/frontend/views.py
+++ youmu/frontend/views.py
@@ -9,7 +9,7 @@ frontend = Blueprint("frontend", __name__)
 def index():
     return render_template('index.html')
 
-@frontend.route('/video/<video_id>')
+@frontend.route('/videos/<video_id>')
 def display(video_id):
 	return render_template('video.html', video_id = video_id)
 
