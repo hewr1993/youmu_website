1b84b3cf4c847ad7e52ab8130b365d6093793005
Ann Jem <10inbinary@gmail.com>
2014-10-27 20:23:28 +0800

    Add placeholder code for methods.

---
+++ youmu/services/video/service.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+
+"""Backend video management module.
+"""
+
+
+class VideoService():
+
+    @staticmethod
+    def delete_video(video_id):
+        return
+
+    @staticmethod
+    def transcode_video(video_id, params):
+        return
+
+#~ Ignore uploading for now
+#~     @staticmethod
+#~     def upload_video():
+#~         return
