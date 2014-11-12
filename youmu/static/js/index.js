fd89efa1dff404925d9dd602db748a3f2e61ac8c
Ann Jem <10inbinary@gmail.com>
2014-11-12 21:35:10 +0800

    Set up smooth scrolling when navbar sticks.

--- youmu/static/js/index.js
+++ youmu/static/js/index.js
@@ -8,9 +8,11 @@ $(window).load(function() {
     $(window).bind('scroll', function() {
         if ($(window).scrollTop() > navpos.top) {
             $('.navbar').addClass('navbar-fixed');
+            $('.masthead').addClass('masthead-expanded');
         }
         else {
             $('.navbar').removeClass('navbar-fixed');
+            $('.masthead').removeClass('masthead-expanded');
         }
     }); 
 });
