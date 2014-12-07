03200ef85e93549e6b7ac871ea09a428a4cbb3ad
Ann Jem <10inbinary@gmail.com>
2014-12-08 00:56:46 +0800

    Rewrite videolist queries.

--- youmu/api/videolist/service.py
+++ youmu/api/videolist/service.py
@@ -18,32 +18,30 @@ class VideoListService(object):
         return order_by
 
     @staticmethod
+    def return_query_res(query, offset=0, size=10, order_by="upload_time",
+                         reverse=False, show_banned=False, show_disabled=False):
+        order_by = VideoListService.validate_order_by(order_by)
+        if not show_disabled: query["disabled"] = False
+        if not show_banned: query["banned"] = False
+        return [VideoListService.mto(item) for item in mongo.get_ordered_video_list(
+            query, offset, size, order_by, reverse)]
+
+    @staticmethod
     def general_get(offset = 0, size = 10, order_by = "upload_time", reverse = False,
                     show_banned = False, show_disabled = False):
-        order_by = VideoListService.validate_order_by(order_by)
-        query = {}
-        if not show_disabled: query["disabled"] = False
-        if not show_banned: query["banned"] = False
-        return [VideoListService.mto(item) for item in mongo.get_ordered_video_list(
-            query, offset, size, order_by, reverse)]
+        return VideoListService.return_query_res({}, offset, size,
+                order_by, reverse, show_banned, show_disabled)
 
     @staticmethod
-    def get_with_owner(owner_id, offset = 0, size = 10, order_by = "upload_time", reverse = False,
-                       show_banned = False, show_disabled = False):
-        order_by = VideoListService.validate_order_by(order_by)
+    def get_with_owner(owner_id, offset=0, size=10, order_by="upload_time",
+                       reverse=False, show_banned=False, show_disabled=False):
         query = { "owner_id": owner_id }
-        if not show_disabled: query["disabled"] = False
-        if not show_banned: query["banned"] = False
-        return [VideoListService.mto(item) for item in mongo.get_ordered_video_list(
-            query, offset, size, order_by, reverse)]
+        return VideoListService.return_query_res(query, offset, size,
+                order_by, reverse, show_banned, show_disabled)
 
     @staticmethod
-    def query_on_title(title, offset = 0, size = 10, order_by = "upload_time", reverse = False,
-                       show_banned = False, show_disabled = False):
-        order_by = VideoListService.validate_order_by(order_by)
-
+    def query_on_title(title, offset=0, size=10, order_by="upload_time",
+                       reverse=False, show_banned=False, show_disabled=False):
         query = { "title": { "$regex": title, "$options": "i" } }
-        if not show_disabled: query["disabled"] = False
-        if not show_banned: query["banned"] = False
-        return [VideoListService.mto(item) for item in mongo.get_ordered_video_list(
-            query, offset, size, order_by, reverse)]
+        return VideoListService.return_query_res(query, offset, size,
+                order_by, reverse, show_banned, show_disabled)
